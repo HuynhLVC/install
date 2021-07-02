0d8279a33aa41dcee54cfa8c6f87fc7d1ff19607
HuynhLVC <73485358+HuynhLVC@users.noreply.github.com>
2021-07-03 01:58:17 +0700

    Update install.py

--- install.py
+++ install.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import os
+from shutil import copyfile
+
 os.system("git clone https://github.com/HuynhLVC/ui.git")
 
 os.system("sudo apt-get install python3-tk")
@@ -8,6 +10,12 @@ os.system("sudo apt-get install python-tk")
 #
 os.system("mkdir -p /usr/icon")
 os.system("mkdir -p /usr/program")
+
+os.system("mkdir -p /usr/program/Backup")
+copyfile("/etc/wpa_supplicant/wpa_supplicant.conf", "/usr/program/Backup/wpa_supplicant.conf") #WifiBackup
+copyfile("/boot/config.txt", "/usr/program/Backup/config.txt") #DisplayBackup
+copyfile("/etc/xdg/lxsession/LXDE-pi/autostart", "/usr/program/Backup/autostart") #UrlBackup
+#TimeBackup is REMOVE file created to Reset
 
 os.system("sudo mv /home/pi/ui/ui.py /usr/program")
 
